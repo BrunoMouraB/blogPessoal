d157f673597db47b64224eb0d028f53979096ee1
Bruno Moura <brunnomhoura@gmail.com>
2020-09-02 11:50:46 -0300

    Inclusão do post postagens e temas

--- blogPessoalAngular/src/app/feed/feed.component.ts
+++ blogPessoalAngular/src/app/feed/feed.component.ts
@@ -4,12 +4,16 @@ import { PostagemService } from './../service/postagem.service';
 import { Postagem } from './../model/Postagem';
 import { Tema } from '../model/Tema';
 
+
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+
+  key = 'data'
+  reverse = true
 
   postagem: Postagem = new Postagem()
   listaPostagens: Postagem[]
@@ -36,6 +40,22 @@ export class FeedComponent implements OnInit {
     this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
       this.listaPostagens = resp
     })
+  }
+
+  publicar (){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    if (this.postagem.titulo == null || this.postagem.texto == null || this.postagem.tema == null) {
+      alert ('Preencha todos os campos antes de publicar!')
+    } else {
+      this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+        this.postagem = resp
+        this.postagem = new Postagem()
+        alert ('Postagem realizada com sucesso!')
+        this.findAllPostagens()
+      })
+    }
   }
 
   findAllTemas() {
